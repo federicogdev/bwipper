a814c47d8c91f71b4b0f716c5d76d8414e42f9dc
Federico Giorgino <grgfdrc@gmail.com>
2023-03-15 22:39:55 +0100

    serverAuth func to get logged in user

---
+++ libs/serverAuth.ts
@@ -0,0 +1,29 @@
+import { NextApiRequest } from "next";
+import { getSession } from "next-auth/react";
+
+import prisma from "@/libs/prismadb";
+
+const serverAuth = async (req: NextApiRequest) => {
+  //gets the session from next auth
+  const session = await getSession({ req });
+
+  //check if user is logged in
+  if (!session?.user?.email) {
+    throw new Error("Not signed in");
+  }
+  //finds the user in the db
+  const currentUser = await prisma.user.findUnique({
+    where: {
+      email: session.user.email,
+    },
+  });
+
+  //check if theres a  user in the db with the email from session
+  if (!currentUser) {
+    throw new Error("Not signed in");
+  }
+
+  return { currentUser };
+};
+
+export default serverAuth;
